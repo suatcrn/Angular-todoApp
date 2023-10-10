9a7b9fe6cfd6920af758451cba406e8cffa5170f
Suat Ceran <slsmr48@gmail.com>
2023-10-10 20:56:56 +0200

    angTodo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todoApp';
+
+  works:any[]=[]
+  work:string=""
+  updatedWork:string=''
+  index:number=0;
+  addWork(){
+    this.works.push(this.work)
+    this.work=""
+  }
+
+  delFunc(index:number){
+    let result:boolean=confirm("Silmek istediginizden emin misiniz?")
+    if(result){
+      this.works.splice(index,1)
+    }
+   
+  }
+
+  editWork(index:number,work:string){
+    this.updatedWork=work
+    this.index=index
+  }
+
+  update(){
+    this.works[this.index]=this.updatedWork
+    
+  }
+
+
 }
